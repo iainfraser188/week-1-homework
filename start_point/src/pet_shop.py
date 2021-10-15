3325c13dca52d1961f4af042ecf7d76cb51236f3
iain <iainfraser188@msn.com>
2021-10-15 17:23:25 +0100

    fifth soulution

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -11,6 +11,9 @@ def get_total_cash(cash):
 def add_or_remove_cash(num1,num2):
     num1["admin"]["total_cash"] = num2 + num1["admin"]["total_cash"]
     
+def get_pets_sold(sold):
+    return sold["admin"]["pets_sold"]
+
 
 
 
