204475a95d9fa9803ac5be86162a796a85534762
iain <iainfraser188@msn.com>
2021-10-15 17:17:14 +0100

    3rd and fourth solved

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -1,9 +1,17 @@
 # WRITE YOUR FUNCTIONS HERE
+
 def get_pet_shop_name (name):
+
 
     return name["name"]
 
 def get_total_cash(cash):
     return cash["admin"]["total_cash"]
+
+def add_or_remove_cash(num1,num2):
+    num1["admin"]["total_cash"] = num2 + num1["admin"]["total_cash"]
+    
+
+
 
 
